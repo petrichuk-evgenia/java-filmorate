738e7ff1a00b1e0b14209e62f31c690ab3b64625
Сафонов Вадим Андреевич (21648638) <safonov.v.a@sbertech.ru>
2026-02-16 11:05:46 +0300

    add get common films

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/UserDbStorageImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/UserDbStorageImpl.java
@@ -27,13 +27,12 @@ import java.util.Optional;
 public class UserDbStorageImpl implements UserStorage {
     private final JdbcTemplate jdbcTemplate;
     private final FilmDbStorageImpl filmDbStorage;
-    private static final String GET_COMMON_FILMS_QUERY = "SELECT f.film_id, f.name, f.description, f.release_date, f.duration, f.mpa_rating_id, m.name AS mpa_name, (SELECT COUNT(*) FROM film_likes fl WHERE fl.film_id = f.film_id) AS likes_count FROM films f LEFT JOIN mpa_ratings m ON f.mpa_rating_id = m.id WHERE f.film_id IN (SELECT l1.film_id FROM film_likes l1 WHERE l1.user_id = ?) AND f.film_id IN (SELECT l2.film_id FROM film_likes l2 WHERE l2.user_id = ?) ORDER BY likes_count DESC";
+    private static final String GET_COMMON_FILMS_QUERY = "SELECT f.film_id, f.name, f.description, f.release_date, f.duration, f.mpa_id, m.name AS mpa_name, COALESCE(l.likes_count, 0) AS likes_count FROM films f INNER JOIN mpa_ratings m ON f.mpa_id = m.mpa_id LEFT JOIN (SELECT film_id, COUNT(*) AS likes_count FROM likes GROUP BY film_id) l ON f.film_id = l.film_id WHERE f.film_id IN (SELECT film_id FROM likes WHERE user_id = ?) AND f.film_id IN (SELECT film_id FROM likes WHERE user_id = ?) ORDER BY likes_count DESC, f.film_id";
 
     public UserDbStorageImpl(JdbcTemplate jdbcTemplate, FilmDbStorageImpl filmDbStorage) {
         this.jdbcTemplate = jdbcTemplate;
         this.filmDbStorage = filmDbStorage;
     }
-
 
     @Override
     public List<User> getAllUsers() {
