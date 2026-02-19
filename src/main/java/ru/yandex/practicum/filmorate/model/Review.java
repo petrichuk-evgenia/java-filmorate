ef472765d722e165da714aef4bbbe0a01a5c4471
Petrichuk.E.V <Petrichuk.E.V@sbertech.ru>
2026-02-20 04:49:28 +0700

    add review

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -0,0 +1,33 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+    private Long reviewId;
+
+    @NotBlank(message = "Content cannot be blank")
+    private String content;
+
+    private Boolean isPositive;
+
+    @NotNull(message = "UserId cannot be null")
+    private Long userId;
+
+    @NotNull(message = "FilmId cannot be null")
+    private Long filmId;
+
+    private Integer useful;
+
+    private LocalDateTime createdAt;
+}
