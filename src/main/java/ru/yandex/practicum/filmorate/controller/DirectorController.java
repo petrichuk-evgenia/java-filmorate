b66779dc537fd88d8165ac087ad6a9f8031aa249
Petrichuk.E.V <Petrichuk.E.V@sbertech.ru>
2026-02-21 14:43:48 +0700

    Исправление тестов

--- src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Director;
@@ -29,7 +30,7 @@ public class DirectorController {
     }
 
     @PostMapping
-    public Director createDirector(@RequestBody Director director) {
+    public Director createDirector(@RequestBody @NonNull Director director) {
         return directorService.createDirector(director);
     }
 
