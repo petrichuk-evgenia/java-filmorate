f286a556a43525964903e30ab965e938f8206731
Petrichuk.E.V <Petrichuk.E.V@sbertech.ru>
2026-02-22 15:57:19 +0700

    Исправление тестов

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/ReviewDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/ReviewDbStorage.java
@@ -49,8 +49,18 @@ public class ReviewDbStorage {
     }
 
     public Review update(Review review) {
-        String sql = "UPDATE reviews SET content = ?, is_positive = ?, useful = ? WHERE review_id = ?";
-        jdbcTemplate.update(sql, review.getContent(), review.getIsPositive(), review.getUseful(), review.getReviewId());
+        String sql = "UPDATE reviews SET content = ?, " +
+                "is_positive = ?, " +
+                "user_id = ?, " +
+                "film_id = ?, " +
+                "useful = ? " +
+                "WHERE review_id = ?";
+        jdbcTemplate.update(sql, review.getContent(),
+                review.getIsPositive(),
+                review.getUserId(),
+                review.getFilmId(),
+                review.getUseful(),
+                review.getReviewId());
         return getById(review.getReviewId()).orElseThrow();
     }
 
