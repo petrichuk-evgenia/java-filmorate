67749ea0904946394bd787634b58e851cf4a7a9a
Petrichuk.E.V <Petrichuk.E.V@sbertech.ru>
2026-02-22 08:09:34 +0700

    Исправление тестов

--- src/main/java/ru/yandex/practicum/filmorate/model/Director.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -1,11 +1,17 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class Director {
+
     private Long id;
+
+    @NotBlank(message = "Логин не может быть пустым")
+    @Pattern(regexp = "\\S+", message = "Логин не может содержать пробелы")
     private String name;
 }
