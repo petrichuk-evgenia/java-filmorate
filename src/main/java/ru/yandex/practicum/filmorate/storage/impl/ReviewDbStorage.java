f04b8d18aa45ce6967cb0b11a552ae1d63490991
Petrichuk.E.V <Petrichuk.E.V@sbertech.ru>
2026-02-22 15:17:44 +0700

    Исправление тестов

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/ReviewDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/ReviewDbStorage.java
@@ -76,7 +76,7 @@ public class ReviewDbStorage {
     public List<Review> getAll(int count) {
         String sql = "SELECT * FROM reviews " +
                 //"ORDER BY useful DESC, created_at DESC " +
-                "ORDER BY useful DESC " +
+                "ORDER BY created_at DESC " +
                 "LIMIT ?";
         return jdbcTemplate.query(sql, reviewRowMapper, count);
     }
